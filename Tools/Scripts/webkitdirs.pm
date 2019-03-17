da6d567b19baddbae70ea6093acda01284c8d940
RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
2019-03-17 08:40:40 +0000

    Enable MiniBrowser, launch subprocesses, load blank URL
    - Added message handler to main run loop and made changes to webview structure

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -3362,6 +3362,10 @@ sub runMiniBrowser
     if (isIOSWebKit()) {
         return runIOSWebKitApp(mobileMiniBrowserBundle());
     }
+    if(isHaiku()){
+       my $webKitLauncherPath = File::Spec->catfile(productDir(), "MiniBrowser");
+       return system {  $webKitLauncherPath } $webKitLauncherPath, @ARGV;
+    }
     return 1;
 }
 
