68d0d0f6e0542494b1109c570d022a87459e6c83
RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
2019-06-22 17:00:15 +0000

    Fixed Shared Memory and Started Network Process implementation

--- Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
+++ Source/WebKit/NetworkProcess/NetworkResourceLoader.cpp
@@ -1564,6 +1564,7 @@ void NetworkResourceLoader::startBufferingTimerIfNeeded()
     if (m_bufferingTimer.isActive())
         return;
     m_bufferingTimer.startOneShot(m_parameters.maximumBufferingTime);
+
 }
 
 void NetworkResourceLoader::bufferingTimerFired()
