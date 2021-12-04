505623ab806188a6b5fbe8a1a6606df302202ab9
Adrien Destugues <pulkomandy@gmail.com>
2021-12-04 21:58:48 +0100

    Force linking of WTF into WebKit
    Not sure why the cross platform way to do it isn't working for us...

--- Source/WebKit/PlatformHaiku.cmake
+++ Source/WebKit/PlatformHaiku.cmake
@@ -106,6 +106,7 @@ list(APPEND WebKit_LIBRARIES
     ${OPENGL_LIBRARIES}
     ${PNG_LIBRARIES}
     ${SQLITE_LIBRARIES}
+    -Wl,--whole-archive WTF -Wl,--no-whole-archive
 )
 
 list(APPEND WebProcess_SOURCES
@@ -122,6 +123,8 @@ list(APPEND WebProcess_LIBRARIES
     ${OPENGL_LIBRARIES}
     ${SQLITE_LIBRARIES}
 )
+
+add_definitions(-DWEBKIT2_COMPILATION)
 
 add_custom_target(forwarding-headerHaiku
     COMMAND ${PERL_EXECUTABLE} ${WEBKIT_DIR}/Scripts/generate-forwarding-headers.pl ${WEBKIT_DIR} ${DERIVED_SOURCES_WEBKIT_DIR}/include haiku
