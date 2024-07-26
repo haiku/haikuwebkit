d128c77deeea0c68b6b1c1488568e67eba1acbca
Zardshard <0azrune6@zard.anonaddy.com>
2024-07-26 11:58:48 -0400

    Cleanup

--- Source/WebKit/UIProcess/API/haiku/PageClientImplHaiku.cpp
+++ Source/WebKit/UIProcess/API/haiku/PageClientImplHaiku.cpp
@@ -36,307 +36,307 @@
 
 namespace WebKit
 {
-    using namespace WebCore;
+using namespace WebCore;
 
-    PageClientImpl::PageClientImpl(WebViewBase& view)
-        : fWebView(view)
-    {
-    }
+PageClientImpl::PageClientImpl(WebViewBase& view)
+    : fWebView(view)
+{
+}
 
-    std::unique_ptr<DrawingAreaProxy> PageClientImpl::createDrawingAreaProxy(WebKit::WebProcessProxy& processProxy)
-    {
+std::unique_ptr<DrawingAreaProxy> PageClientImpl::createDrawingAreaProxy(WebKit::WebProcessProxy& processProxy)
+{
 #if USE(COORDINATED_GRAPHICS) || USE(TEXTURE_MAPPER)
-        return std::make_unique<DrawingAreaProxyCoordinatedGraphics>(*fWebView.page(), processProxy);
+    return std::make_unique<DrawingAreaProxyCoordinatedGraphics>(*fWebView.page(), processProxy);
 #else
-        // This will likely cause a crash from a failing assert. At least it's
-        // better than crashing because of not returning anything
-        return nullptr;
+    // This will likely cause a crash from a failing assert. At least it's
+    // better than crashing because of not returning anything
+    return nullptr;
 #endif
-    }
+}
 
-    void PageClientImpl::setViewNeedsDisplay(const WebCore::Region& region)
-    {
-        if (fWebView.LockLooper()) {
-            fWebView.Invalidate(region.bounds());
-            fWebView.UnlockLooper();
-        }
+void PageClientImpl::setViewNeedsDisplay(const WebCore::Region& region)
+{
+    if (fWebView.LockLooper()) {
+        fWebView.Invalidate(region.bounds());
+        fWebView.UnlockLooper();
     }
+}
 
-    void PageClientImpl::requestScroll(const WebCore::FloatPoint&, const WebCore::IntPoint&, WebCore::ScrollIsAnimated)
-    {
-        notImplemented();
-    }
+void PageClientImpl::requestScroll(const WebCore::FloatPoint&, const WebCore::IntPoint&, WebCore::ScrollIsAnimated)
+{
+    notImplemented();
+}
 
-    WebCore::FloatPoint PageClientImpl::viewScrollPosition()
-    {
-        notImplemented();
-        return { };
-    }
+WebCore::FloatPoint PageClientImpl::viewScrollPosition()
+{
+    notImplemented();
+    return { };
+}
 
-    WebCore::IntSize PageClientImpl::viewSize()
-    {
-        fWebView.Window()->Lock();
-        BRect rect = fWebView.Frame();
-        fWebView.Window()->Unlock();
-        return IntSize(rect.right - rect.left, rect.bottom - rect.top);
-    }
+WebCore::IntSize PageClientImpl::viewSize()
+{
+    fWebView.Window()->Lock();
+    BRect rect = fWebView.Frame();
+    fWebView.Window()->Unlock();
+    return IntSize(rect.right - rect.left, rect.bottom - rect.top);
+}
 
-    bool PageClientImpl::isViewWindowActive()
-    {
-        //return fWebView.isWindowActive();
-        return false;
-    }
+bool PageClientImpl::isViewWindowActive()
+{
+    //return fWebView.isWindowActive();
+    return false;
+}
 
-    bool PageClientImpl::isViewFocused()
-    {
-        //return fWebView.isFocused();
-        return false;
-    }
+bool PageClientImpl::isViewFocused()
+{
+    //return fWebView.isFocused();
+    return false;
+}
 
-    bool PageClientImpl::isViewVisible()
-    {
-        //return fWebView.isVisible();
-        return true;
-    }
+bool PageClientImpl::isViewVisible()
+{
+    //return fWebView.isVisible();
+    return true;
+}
 
-    bool PageClientImpl::isViewInWindow()
-    {
-        //return fWebView.isInWindow();
-        return false;
-    }
+bool PageClientImpl::isViewInWindow()
+{
+    //return fWebView.isInWindow();
+    return false;
+}
 
-    void PageClientImpl::PageClientImpl::processDidExit()
-    {
-        notImplemented();
-    }
+void PageClientImpl::PageClientImpl::processDidExit()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didRelaunchProcess()
-    {
-        notImplemented();
-    }
+void PageClientImpl::didRelaunchProcess()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::toolTipChanged(const String&, const String& newToolTip)
-    {
-        //fWebView.setToolTip(newToolTip);
-    }
+void PageClientImpl::toolTipChanged(const String&, const String& newToolTip)
+{
+    //fWebView.setToolTip(newToolTip);
+}
 
-    void PageClientImpl::setCursor(const WebCore::Cursor& cursor)
-    {
-        //fWebView.setCursor(cursor);
-    }
+void PageClientImpl::setCursor(const WebCore::Cursor& cursor)
+{
+    //fWebView.setCursor(cursor);
+}
 
-    void PageClientImpl::setCursorHiddenUntilMouseMoves(bool /* hiddenUntilMouseMoves */)
-    {
-        notImplemented();
-    }
+void PageClientImpl::setCursorHiddenUntilMouseMoves(bool /* hiddenUntilMouseMoves */)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didChangeViewportProperties(const WebCore::ViewportAttributes&)
-    {
-        notImplemented();
-    }
+void PageClientImpl::didChangeViewportProperties(const WebCore::ViewportAttributes&)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::registerEditCommand(Ref<WebEditCommandProxy>&& command, UndoOrRedo undoOrRedo)
-    {
-        fUndoController.registerEditCommand(WTFMove(command), undoOrRedo);
-    }
+void PageClientImpl::registerEditCommand(Ref<WebEditCommandProxy>&& command, UndoOrRedo undoOrRedo)
+{
+    fUndoController.registerEditCommand(WTFMove(command), undoOrRedo);
+}
 
-    void PageClientImpl::clearAllEditCommands()
-    {
-        fUndoController.clearAllEditCommands();
-    }
+void PageClientImpl::clearAllEditCommands()
+{
+    fUndoController.clearAllEditCommands();
+}
 
-    bool PageClientImpl::canUndoRedo(UndoOrRedo undoOrRedo)
-    {
-        return fUndoController.canUndoRedo(undoOrRedo);
-    }
+bool PageClientImpl::canUndoRedo(UndoOrRedo undoOrRedo)
+{
+    return fUndoController.canUndoRedo(undoOrRedo);
+}
 
-    void PageClientImpl::executeUndoRedo(UndoOrRedo undoOrRedo)
-    {
-        fUndoController.executeUndoRedo(undoOrRedo);
-    }
+void PageClientImpl::executeUndoRedo(UndoOrRedo undoOrRedo)
+{
+    fUndoController.executeUndoRedo(undoOrRedo);
+}
 
-    FloatRect PageClientImpl::convertToDeviceSpace(const FloatRect& viewRect)
-    {
-        notImplemented();
-        return viewRect;
-    }
+FloatRect PageClientImpl::convertToDeviceSpace(const FloatRect& viewRect)
+{
+    notImplemented();
+    return viewRect;
+}
 
-    FloatRect PageClientImpl::convertToUserSpace(const FloatRect& viewRect)
-    {
-        notImplemented();
-        return viewRect;
-    }
+FloatRect PageClientImpl::convertToUserSpace(const FloatRect& viewRect)
+{
+    notImplemented();
+    return viewRect;
+}
 
-    IntPoint PageClientImpl::screenToRootView(const IntPoint& point)
-    {
-        return IntPoint();
-    }
+IntPoint PageClientImpl::screenToRootView(const IntPoint& point)
+{
+    return IntPoint();
+}
 
-    IntRect PageClientImpl::rootViewToScreen(const IntRect& rect)
-    {
-        return IntRect();
-    }
+IntRect PageClientImpl::rootViewToScreen(const IntRect& rect)
+{
+    return IntRect();
+}
 
-    void PageClientImpl::doneWithKeyEvent(const NativeWebKeyboardEvent& event, bool wasEventHandled)
-    {
-        notImplemented();
-    }
+void PageClientImpl::doneWithKeyEvent(const NativeWebKeyboardEvent& event, bool wasEventHandled)
+{
+    notImplemented();
+}
 
-    RefPtr<WebPopupMenuProxy> PageClientImpl::createPopupMenuProxy(WebPageProxy& page)
-    {
-        notImplemented();
-        //return WebPopupMenuProxyWin::create(&fWebView, page);
-    }
+RefPtr<WebPopupMenuProxy> PageClientImpl::createPopupMenuProxy(WebPageProxy& page)
+{
+    notImplemented();
+    //return WebPopupMenuProxyWin::create(&fWebView, page);
+}
 
 #if ENABLE(CONTEXT_MENUS)
-    Ref<WebContextMenuProxy> PageClientImpl::createContextMenuProxy(WebPageProxy& page, ContextMenuContextData&& context, const UserData& userData)
-    {
-        notImplemented();
-        //return WebContextMenuProxyWin::create(page, WTFMove(context), userData);
-    }
+Ref<WebContextMenuProxy> PageClientImpl::createContextMenuProxy(WebPageProxy& page, ContextMenuContextData&& context, const UserData& userData)
+{
+    notImplemented();
+    //return WebContextMenuProxyWin::create(page, WTFMove(context), userData);
+}
 #endif
 
-    RefPtr<WebColorPicker> PageClientImpl::createColorPicker(WebPageProxy*, const WebCore::Color& intialColor,
-        const WebCore::IntRect&, Vector<WebCore::Color>&&)
-    {
-        return nullptr;
-    }
+RefPtr<WebColorPicker> PageClientImpl::createColorPicker(WebPageProxy*, const WebCore::Color& intialColor,
+    const WebCore::IntRect&, Vector<WebCore::Color>&&)
+{
+    return nullptr;
+}
 
-    void PageClientImpl::enterAcceleratedCompositingMode(const LayerTreeContext& layerTreeContext)
-    {
-        notImplemented();
-    }
+void PageClientImpl::enterAcceleratedCompositingMode(const LayerTreeContext& layerTreeContext)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::exitAcceleratedCompositingMode()
-    {
-        notImplemented();
-    }
+void PageClientImpl::exitAcceleratedCompositingMode()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::updateAcceleratedCompositingMode(const LayerTreeContext& layerTreeContext)
-    {
-        notImplemented();
-    }
+void PageClientImpl::updateAcceleratedCompositingMode(const LayerTreeContext& layerTreeContext)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::pageClosed()
-    {
-        notImplemented();
-    }
+void PageClientImpl::pageClosed()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::preferencesDidChange()
-    {
-        notImplemented();
-    }
+void PageClientImpl::preferencesDidChange()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didChangeContentSize(const IntSize& size)
-    {
-        notImplemented();
-    }
+void PageClientImpl::didChangeContentSize(const IntSize& size)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didCommitLoadForMainFrame(const String& /* mimeType */, bool /* useCustomContentProvider */ )
-    {
-        notImplemented();
-    }
+void PageClientImpl::didCommitLoadForMainFrame(const String& /* mimeType */, bool /* useCustomContentProvider */ )
+{
+    notImplemented();
+}
 
-    void PageClientImpl::wheelEventWasNotHandledByWebCore(const NativeWebWheelEvent& event)
-    {
-        notImplemented();
-    }
+void PageClientImpl::wheelEventWasNotHandledByWebCore(const NativeWebWheelEvent& event)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didFinishLoadingDataForCustomContentProvider(const String&, std::span<const unsigned char>)
-    {
-        notImplemented();
-    }
+void PageClientImpl::didFinishLoadingDataForCustomContentProvider(const String&, std::span<const unsigned char>)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::navigationGestureDidBegin()
-    {
-        notImplemented();
-    }
+void PageClientImpl::navigationGestureDidBegin()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::navigationGestureWillEnd(bool, WebBackForwardListItem&)
-    {
-        notImplemented();
-    }
+void PageClientImpl::navigationGestureWillEnd(bool, WebBackForwardListItem&)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::navigationGestureDidEnd(bool, WebBackForwardListItem&)
-    {
-        notImplemented();
-    }
+void PageClientImpl::navigationGestureDidEnd(bool, WebBackForwardListItem&)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::navigationGestureDidEnd()
-    {
-        notImplemented();
-    }
+void PageClientImpl::navigationGestureDidEnd()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::willRecordNavigationSnapshot(WebBackForwardListItem&)
-    {
-        notImplemented();
-    }
+void PageClientImpl::willRecordNavigationSnapshot(WebBackForwardListItem&)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didRemoveNavigationGestureSnapshot()
-    {
-        notImplemented();
-    }
+void PageClientImpl::didRemoveNavigationGestureSnapshot()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didFirstVisuallyNonEmptyLayoutForMainFrame()
-    {
-        notImplemented();
-    }
+void PageClientImpl::didFirstVisuallyNonEmptyLayoutForMainFrame()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didFinishNavigation(API::Navigation*)
-    {
-    }
+void PageClientImpl::didFinishNavigation(API::Navigation*)
+{
+}
 
-    void PageClientImpl::didFailNavigation(API::Navigation*)
-    {
-    }
+void PageClientImpl::didFailNavigation(API::Navigation*)
+{
+}
 
-    void PageClientImpl::didSameDocumentNavigationForMainFrame(SameDocumentNavigationType)
-    {
-        notImplemented();
-    }
+void PageClientImpl::didSameDocumentNavigationForMainFrame(SameDocumentNavigationType)
+{
+    notImplemented();
+}
 
-    void PageClientImpl::didChangeBackgroundColor()
-    {
-        notImplemented();
-    }
+void PageClientImpl::didChangeBackgroundColor()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::isPlayingAudioWillChange()
-    {
-        notImplemented();
-    }
+void PageClientImpl::isPlayingAudioWillChange()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::isPlayingAudioDidChange()
-    {
-        notImplemented();
-    }
+void PageClientImpl::isPlayingAudioDidChange()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::refView()
-    {
-        notImplemented();
-    }
+void PageClientImpl::refView()
+{
+    notImplemented();
+}
 
-    void PageClientImpl::derefView()
-    {
-        notImplemented();
-    }
+void PageClientImpl::derefView()
+{
+    notImplemented();
+}
 
-    WebViewBase* PageClientImpl::viewWidget()
-    {
-        return &fWebView;
-    }
+WebViewBase* PageClientImpl::viewWidget()
+{
+    return &fWebView;
+}
 
 #if ENABLE(DATE_AND_TIME_INPUT_TYPES)
-    RefPtr<WebDateTimePicker> PageClientImpl::createDateTimePicker(WebPageProxy& page)
-    {
-        //return WebDateTimePickerHaiku::create(page);
-        return nullptr;
-    }
+RefPtr<WebDateTimePicker> PageClientImpl::createDateTimePicker(WebPageProxy& page)
+{
+    //return WebDateTimePickerHaiku::create(page);
+    return nullptr;
+}
 #endif
 
 #if ENABLE(FULLSCREEN_API)
-    WebFullScreenManagerProxyClient& PageClientImpl::fullScreenManagerProxyClient()
-    {
-        //return *this;
-    }
+WebFullScreenManagerProxyClient& PageClientImpl::fullScreenManagerProxyClient()
+{
+    //return *this;
+}
 #endif
 }
