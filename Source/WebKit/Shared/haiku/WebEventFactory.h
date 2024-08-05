53bad7ef5ffb9b34dd50d7ff2f3734d7ba1c02db
Zardshard <0azrune6@zard.anonaddy.com>
2024-08-05 11:50:44 -0400

    Cleanup

--- Source/WebKit/Shared/haiku/WebEventFactory.h
+++ Source/WebKit/Shared/haiku/WebEventFactory.h
@@ -23,8 +23,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WebEventFactory_h
-#define WebEventFactory_h
+#pragma once
 
 #include "WebMouseEvent.h"
 
@@ -47,5 +46,3 @@ private:
 };
 
 } // namespace WebKit
-
-#endif // WebEventFactory_h
