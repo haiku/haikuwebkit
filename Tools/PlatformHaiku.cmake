e8c1cf312be22f8638ad651f5e24d59e6d4c3e96
nephele <nep-webkit@packageloss.eu>
2024-10-11 17:04:21 +0200

    Disable WebKitLegacy

--- Tools/PlatformHaiku.cmake
+++ Tools/PlatformHaiku.cmake
@@ -1,5 +1,8 @@
 add_subdirectory(TestRunnerShared)
-add_subdirectory(DumpRenderTree)
-add_subdirectory(HaikuLauncher)
 add_subdirectory(ImageDiff)
 add_subdirectory(MiniBrowser/haiku)
+
+if(ENABLE_WEBKIT_LEGACY EQUAL ON)
+	add_subdirectory(DumpRenderTree)
+	add_subdirectory(HaikuLauncher)
+endif()
