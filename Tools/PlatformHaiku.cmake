b052d895245dc6d3f20f35ed12410c22969575c2
Kacper Kasper <kacperkasper@gmail.com>
2026-01-03 08:38:38 +0100

    Cleanup PlatformHaiku.cmake

--- Tools/PlatformHaiku.cmake
+++ Tools/PlatformHaiku.cmake
@@ -1,8 +1,7 @@
-add_subdirectory(TestRunnerShared)
-add_subdirectory(ImageDiff)
-add_subdirectory(MiniBrowser/haiku)
+if(ENABLE_MINIBROWSER EQUAL ON)
+    add_subdirectory(MiniBrowser/haiku)
+endif()
 
 if(ENABLE_WEBKIT_LEGACY EQUAL ON)
-    add_subdirectory(DumpRenderTree)
     add_subdirectory(HaikuLauncher)
 endif()
